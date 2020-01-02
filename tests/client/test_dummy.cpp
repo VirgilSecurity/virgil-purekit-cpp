bfba8c9e0de803bc3c5fd20c2f956a81fba007fa
Oleksandr Fryziuk <fryzukalex@gmail.com>
2020-01-02 13:07:13 +0200

    Add curlpp lib

--- tests/client/test_dummy.cpp
+++ tests/client/test_dummy.cpp
@@ -2,6 +2,10 @@
 #include "vscf_sha512.h"
 #include "vscf_hash.h"
 
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
 TEST(One, EqualsOne) {
     EXPECT_EQ(1, 1);
 }
@@ -13,4 +17,33 @@ TEST(HASH, Sha512Works) {
     EXPECT_TRUE( impl != NULL );
 
     vscf_sha512_destroy(&sha512);
+}
+
+
+TEST(CURL, DummyCurl) {
+    try
+    {
+        // That's all that is needed to do cleanup of used resources (RAII style).
+        curlpp::Cleanup myCleanup;
+
+        // Our request to be sent.
+        curlpp::Easy myRequest;
+
+        // Set the URL.
+        myRequest.setOpt<curlpp::options::Url>("http://example.com");
+
+        // Send request and get a result.
+        // By default the result goes to standard output.
+        myRequest.perform();
+    }
+
+    catch(curlpp::RuntimeError & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    catch(curlpp::LogicError & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
